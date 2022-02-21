803860d59f406706e2932e153445de1a86385dad
Isabell Orlishausen <orlishausen@cip.ifi.lmu.de>
2022-02-21 11:54:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# pprint
+## Setup
+For development, create virtual environment with `python3 -m venv venv`. Later use `source venv/bin/activate`.
+Install requirements with `pip install -r requirements/dev.txt`.
---
+++ ppprint/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Job(models.Model):
+    class StatusChoices(models.TextChoices):
+        CREATED = "CREATED", _("Created")
+        RUNNING = "RUNNING", _("Running")
+        SUCCESS = "SUCCESS", _("Success")
+        FAILURE = "FAILURE", _("Failure")
+
+    status = models.CharField(max_length=7, choices=StatusChoices.choices)
+
+    class Meta:
+        abstract = True
+
+
+class ImportJob(Job):
+    proteome = models.ForeignKey("Proteome", on_delete=models.CASCADE)
+
+
+class DataSource(models.Model):
+    proteome = models.ForeignKey("Proteome", on_delete=models.CASCADE)
+
+
+class VisualizationJob(Job):
+    sources = models.ManyToManyField("DataSource")
+
+
+class Proteome(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    uniprot_id = models.CharField(max_length=11, blank=True, default="")
+    description = models.TextField(blank=True, default="")
